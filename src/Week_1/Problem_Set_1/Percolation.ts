734211fc153be88a3f69f830d78f60ef01999ecf
Abdul Shabbir <abdulqshabbir@gmail.com>
2020-08-04 16:34:20 +0800

    Week 1 problem set: Percolation class completed.

--- src/Week_1/Problem_Set_1/Percolation.ts
+++ src/Week_1/Problem_Set_1/Percolation.ts
@@ -1,69 +1,114 @@
-interface Site {
-    isOpen: boolean
-    row: number
-    col: number
-}
-
-class Site {
-    constructor(row: number, col: number) {
-        this.row = row
-        this.col = col
-        this.isOpen = false
-    }
-}
+import { WeightedQuickUnion } from '../WeightedQuickUnion'
 
 interface Percolation {
-    grid: Site[][],
-    squareGridLength: number,
-    numOfOpenSites: number
+    grid: WeightedQuickUnion,
+    gridLength: number
+    numOfOpenSites: number,
 }
 
 class Percolation {
-    constructor(N: number) {
-        this.grid = this.initialize(N)
-        this.squareGridLength = N
+    constructor(n: number) {
+        /*
+            The 2D grid will be represented with a 1D array
+            where the index represents a particular site and
+            the entry represents that site's connected parent
+
+            The indices from 0 to n^2 - 1 will represent the sites.
+        */
+        this.grid = new WeightedQuickUnion(n ** 2)
         this.numOfOpenSites = 0
+        this.gridLength = n
     }
-    private initialize(N: number): Site[][] {
-        let grid: Site[][] = []
-        for (let i = 1; i <= N; i++) {
-            grid[i] = []
-            for (let j = 1; j <= N; j++) {
-                grid[i][j] = new Site(i, j)
+    public percolates(): boolean {
+        let lastRow = this.gridLength
+        let lastCol = this.gridLength
+
+        for (let col = 1; col <= lastCol; col++) {
+            if (this.isFull(lastRow, col)) {
+                return true
             }
         }
-        return grid
+        return false
     }
-    private isValidSite(row: number, col: number): boolean {
-        if (row <= 0 || row > this.squareGridLength) {
-            return false
+    public isFull(row: number, col: number): boolean {
+        let current = this.getIndexOfSite(row, col)
+        if (!this.isValidIndex(current)) throw new Error("Invalid argument.")
+
+        // if site is not open, it cannot be connected to a site in the top row so return false
+        if (!this.isOpen(row, col)) return false
+
+        for (let i = 0; i < this.gridLength; i++) {
+            // let i represent an index of a site in the top row
+            if (this.grid.connected(i, current)) {
+                return true
+            }
         }
-        if (col <= 0 || col > this.squareGridLength) {
-            return false
-        }
-        return true
+        return false
     }
-    open(row: number, col: number): void {
-        if (this.isValidSite(row, col) && !this.grid[row][col].isOpen) {
-            this.grid[row][col].isOpen = true
+    public open(row: number, col: number) {
+        let current = this.getIndexOfSite(row, col)
+
+        // if invalid site, return
+        if (!this.isValidIndex(current)) throw new Error("Invalid Argument.")
+
+        // if site is not open, open it
+        if (!this.grid.open[current]) {
+            this.grid.open[current] = true
             this.numOfOpenSites++
         }
-        this.print()
-    }
 
-    isOpen(row: number, col: number): boolean {
-        if (!this.isValidSite(row, col)) {
-            return false
-        }
-        return this.grid[row][col].isOpen
-    }
+        let neighbours = this.getOpenNeighbours(row, col)
 
-    numberOfOpenSites(): number {
+        // For each neighbour that is open, connect current site to neighbouring site
+        neighbours.forEach(neighbour => {
+            if (this.grid.open[neighbour] === true) {
+                this.grid.union(current, neighbour)
+            }
+        })
+    }
+    public isOpen(row: number, col: number): boolean {
+        let idx = this.getIndexOfSite(row, col)
+
+        if (!this.isValidIndex(idx)) throw new Error("Invalid argument.")
+
+        return this.grid.open[idx]
+    }
+    public numberOfOpenSites(): number {
         return this.numOfOpenSites
     }
-    print() {
-        console.log(this.grid)
+    private getOpenNeighbours(row: number, col: number): number[] {
+        let neighbours = [] // the numbers in this array represent the position of the neighbour site in the grid
+
+        let upIndex = this.getIndexOfSite(row - 1, col)
+        let downIndex = this.getIndexOfSite(row + 1, col)
+        let leftIndex = this.getIndexOfSite(row, col - 1)
+        let rightIndex = this.getIndexOfSite(row, col + 1)
+
+        if (this.isValidIndex(upIndex) && this.grid.open[upIndex]) {
+            neighbours.push(upIndex)
+        }
+
+        if (this.isValidIndex(downIndex) && this.grid.open[downIndex]) {
+            neighbours.push(downIndex)
+        }
+
+        if (this.isValidIndex(leftIndex) && this.grid.open[leftIndex]) {
+            neighbours.push(leftIndex)
+        }
+
+        if (this.isValidIndex(rightIndex) && this.grid.open[rightIndex]) {
+            neighbours.push(rightIndex)
+        }
+        return neighbours
+    }
+    private isValidIndex(idx: number): boolean {
+        if (idx >= 0 && idx < this.grid.parent.length) return true
+
+        return false
+    }
+    private getIndexOfSite(row: number, col: number) {
+        // converts a (row, col) on a 2D grid to a an index for a 1D array
+        let index = this.gridLength * (row - 1) + (col - 1)
+        return index
     }
 }
-
-let p = new Percolation(4)
--- src/Week_1/WeightedQuickUnion.ts
+++ src/Week_1/WeightedQuickUnion.ts
@@ -2,42 +2,47 @@
 
 /* 
 
-QuickUnion class accepts N objects and each object is 
+WeightedQuickUnion class accepts N objects and each object is 
 represented from 0 to N-1 as the indices of an array. 
 The array entries represent the parent of that object.  In this way
 the array represents a 'forest' (set of trees) whose root node can be
 found by indexing into the array repeatedly until i === id[i] (object is its own 
 parent and is therefore a root)
 
-Method 'union(p, q)' connects the objects p and q by merging the root of p with the root of q
-in an arbitrary order.
+Method 'union(p, q)' connects the objects p and q by merging the root of p with the root of q.  In WeightedQuickUnion
+the root of the larger tree always becomes the parent of the root of the smaller tree.
 
 Method 'connected(p, q)' checks if the two objects: p and q share a root which is equivalent to checking
 if the two obects are connected
 */
 
-interface WeightedQuickUnion {
+export interface WeightedQuickUnion {
     parent: number[]
     size: number[]
+    open: boolean[]
 }
 
-class WeightedQuickUnion {
+export class WeightedQuickUnion {
     constructor(N: number) {
         this.parent = this.initialize(N).parent// ith entry is the parent of ith index
         this.size = this.initialize(N).size
-
+        this.open = this.initialize(N).isOpen
     }
     initialize(N: number) {
         let parent = []
         let size = []
+        let isOpen = []
+
         for (let i = 0; i < N; i++) {
             // make each object its own parent
             parent[i] = i
             size[i] = 1
+            isOpen[i] = false
         }
         return {
             parent,
-            size
+            size,
+            isOpen
         }
     }
     isValidObject(p: number): boolean {
@@ -59,16 +64,6 @@ class WeightedQuickUnion {
         let qRoot = this.getRootOf(q)
 
         if (pRoot === null || qRoot === null) throw new Error('Cannot find root.')
-
-        /*
-            IF size[q] < size[p]:
-                make qRoot a child of pRoot (make pRoot is a parent of qRoot)
-                make size[pRoot] += size[qRoot] 
-            
-            ELSE:
-                make pRoot a child of qRoot
-                size[qRoot] += size[pRoot]
-        */
 
         if (this.size[qRoot] < this.size[pRoot]) {
             this.parent[qRoot] = pRoot
