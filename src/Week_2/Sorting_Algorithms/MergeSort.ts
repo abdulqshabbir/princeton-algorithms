bd6cf34650d45a7255ba1cd5195b79cb1c02d858
Abdul Shabbir <abdulqshabbir@gmail.com>
2020-08-10 17:27:53 +0800

    Merging function for mergeSort now working.

--- src/Week_2/Sorting_Algorithms/MergeSort.ts
+++ src/Week_2/Sorting_Algorithms/MergeSort.ts
@@ -1,33 +1,35 @@
 export function merge(arr: number[], low: number, mid: number, hi: number) {
     /*
-        pre-condition: arr[0...mid] and arr[mid + 1... hi] must be sorted subarrays
+        pre-condition: a[0...mid] and a[mid + 1... hi] are two sorted sublists
     */
-    debugger;
-    let aux = []
+    let aux: number[] = []
+    let i: number, j: number, k: number
+    // i will be an index into the arr from low to mid
+    // j will be an index into the arr from mid + 1 to hi
+    // k will be an index into arr and all elements arr[low] ... a[hi] will be sorted in place
 
-    for (let k = 0; k < arr.length; k++) {
-        aux[k] = arr[k]
+    for (let m = low; m <= hi; m++) {
+        // copy entries of arr[low..hi] to aux.  We will use aux entries to make comparisons for merging in the correct order.
+        aux[m] = arr[m]
     }
 
-    for (let k = 0; k < arr.length; k++) {
-        let i = 0
-        let j = mid + 1
-
+    i = low
+    j = mid + 1
+    k = low
+    while (i <= mid || j <= hi) {
+        // WHILE i and j are valid indices
         if (i > mid) {
-            arr[k] = aux[j]
-            j++
-        } else if (j > hi) {
-            arr[k] = aux[i]
-            i++
-        } else if (arr[i] <= arr[j]) {
-            arr[k] = aux[i]
-            i++
+            // i is not a valid index meaning entries from low .. mid have already been added
+            arr[k++] = aux[j++]
+        }
+        else if (j > hi) {
+            arr[k++] = aux[i++]
+        }
+        else if (aux[i] <= aux[j]) {
+            arr[k++] = aux[i++]
         } else {
-            arr[k] = aux[j]
-            j++
+            arr[k++] = aux[j++]
         }
     }
     return arr
 }
-
-console.log(merge([1, 3, 5, 6, 7, 10], 0, 2, 5))
